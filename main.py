daf80a64e7d66f196764019959cd99b9d7e1d51a
João Vitor <43214611+vitorjoaom@users.noreply.github.com>
2023-12-12 02:35:19 -0300

    2 commit
    Segundo commit dos arquivos cliente e main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from apps import Cliente
+
+def editar(self):
+    self.nome
+    self.nascimento
+    self.email
+    self.conta
+    
+def cadastrar(self):
+    self.nome
+    self.nascimento
+    self.email
+    self.conta
+
+def calcula_idade(self):
+    self.nascimento
+    
